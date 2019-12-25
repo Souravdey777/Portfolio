22d5981a24e4e49dacbb08c0ff4e36b6a419efda
Souravdey777 <piyush.kolkata@gmail.com>
2019-12-26 01:41:12 +0530

    Updates

---
+++ precache-manifest.55c932e5b400c7cf4f809d092d925f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6012d11b0696c909c62f91a05b150d49",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "17cdce6f1b84a32bff08",
+    "url": "/Portfolio/static/css/main.054c5a3e.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/Portfolio/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "17cdce6f1b84a32bff08",
+    "url": "/Portfolio/static/js/main.f7944bfd.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "d398f9e1d57012bc2129d8401558f4a3",
+    "url": "/Portfolio/static/media/profileImage.d398f9e1.jpg"
+  }
+]);
