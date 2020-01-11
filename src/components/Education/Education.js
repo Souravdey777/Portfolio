0a0e6039e642e0530221cd1497adf8d7672357d9
Souravdey777 <piyush.kolkata@gmail.com>
2020-01-11 13:15:38 +0530

    ui fixes

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const Education = () => (
     <div style={{ fontWeight: "bold" }}>
-        <div>Bachelor of Technology (B. Tech)
-                <div style={{ color: "white", fontWeight: "bold" }}> Information Technology.</div>
-            <div style={{ fontSize: "14px", fontWeight: "normal", fontStyle:"italic", textAlign: "right" }}>~2018</div>
-        </div>
+        <div style={{ fontSize: "14px", fontWeight: "normal" }}>2018</div>
+        <div>Bachelor of Technology (B. Tech)</div>
+        <div style={{ color: "white", fontWeight: "bold" }}> Information Technology.</div>
+        <div style={{ fontSize: "14px", fontWeight: "normal" }}>West Bengal University of Technology (WBUT)</div>
         <br />
+        <div style={{ fontSize: "14px", fontWeight: "normal" }}>2014</div>
         <div>Higher Secondary
                 <span> ( Science )</span>
-            <div style={{ fontSize: "14px", fontWeight: "normal", fontStyle:"italic" , textAlign: "right" }}>~2014</div>
         </div>
         <br />
+        <div style={{ fontSize: "14px", fontWeight: "normal" }}>2012</div>
         <div>Secondary
-                <span> ( Science )</span>
-            <div style={{ fontSize: "14px", fontWeight: "normal", fontStyle:"italic" , textAlign: "right" }}>~2012</div>
         </div>
     </div>
 );
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -5,7 +5,7 @@ const Experience = () => (
         <div style={{ fontSize: "14px", fontWeight: "bold" }}>Sept 2018 - Present</div>
         <div style={{ fontSize: "20px", fontWeight: "bold" }}>Tata Consultancy Services</div>
         <div style={{ fontSize: "14px", fontWeight: "bold" }}>Software Developer</div>
-        <ul style={{ fontSize: "14px"}}>
+        <ul style={{ fontSize: "14px", margin: "0" }}>
             <li>hgf du tdjut</li>
             <li>fuyf u yfytd</li>
             <li>hf dru dfu tdf</li>
@@ -13,7 +13,7 @@ const Experience = () => (
         <br />
         <div style={{ fontSize: "14px", fontWeight: "bold" }}>Mar 2017 - Feb 2018</div>
         <div style={{ fontSize: "20px", fontWeight: "bold" }}>Freelancing</div>
-        <ul style={{ fontSize: "14px"}}>
+        <ul style={{ fontSize: "14px", margin: "0" }}>
             <li>hgf du tdjut</li>
             <li>fuyf u yfytd</li>
             <li>hf dru dfu tdf</li>
